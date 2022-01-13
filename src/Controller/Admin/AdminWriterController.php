4a91c988e29beafa34513d3de51bb68bcabe20bd
Edjek <edjek@hotmail.fr>
2022-01-13 16:45:13 +0100

    User and Auth

--- src/Controller/Admin/AdminWriterController.php
+++ src/Controller/Admin/AdminWriterController.php
@@ -2,17 +2,37 @@
 
 namespace App\Controller\Admin;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Writer;
+use App\Form\WriterType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminWriterController extends AbstractController
 {
-    #[Route('/admin/writer', name: 'admin_writer')]
-    public function index(): Response
+    #[Route('/admin/create/writer', name: 'admin_create_writer')]
+    public function createWriter(EntityManagerInterface $entityManagerInterface, Request $request)
     {
-        return $this->render('admin_writer/index.html.twig', [
-            'controller_name' => 'AdminWriterController',
-        ]);
+        $writer = new Writer();
+
+        $writerForm = $this->createForm(WriterType::class, $writer);
+
+        $writerForm->handleRequest($request);
+
+        if ($writerForm->isSubmitted() && $writerForm->isValid()) {
+            $entityManagerInterface->persist($writer);
+            $entityManagerInterface->flush();
+
+            $this->addFlash(
+                'notice',
+                'Une marque a été créé'
+            );
+
+            return $this->redirectToRoute("writer_list");
+        }
+
+        return $this->render("admin/admin_writer/writerform.html.twig", ['writerForm' => $writerForm->createView()]);
     }
 }
